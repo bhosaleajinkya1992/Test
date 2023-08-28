1bada0f382900e2252b5fed19090594f6f115230
bhosaleajinkya1992 <143363715+bhosaleajinkya1992@users.noreply.github.com>
2023-08-28 10:21:31 +0530

    Add files via upload

---
+++ Family/FamilyRecodSystem/WCFServiceApp/Model/MemberData.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WCFServiceApp.Model
+{
+    public class MemberData
+    {
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+        public string MemberMiddleName { get; set; }
+        public string MemberLastName { get; set; }
+        public int Suffix { get; set; }
+        public string DateOfBirth { get; set; }
+        public int Gender { get; set; }
+        public int ApplicantId { get; set; }
+    }
+}
